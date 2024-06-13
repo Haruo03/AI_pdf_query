d39a5d5b8b03ae54261f593a97e9e51f62da78ee
Haruo03 <a22.bd5k@g.chuo-u.ac.jp>
2024-06-13 17:34:37 +0900

    second commit

--- index.py
+++ index.py
@@ -1,16 +1,21 @@
+# pip install pycryptodome
+from glob import glob
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Qdrant
+from langchain.chains import RetrievalQA
 
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 
 QDRANT_PATH = "./local_qdrant"
-COLLECTION_NAME = "my_collection"
+COLLECTION_NAME = "my_collection_2"
 
 
 def init_page():
@@ -22,22 +27,34 @@ def init_page():
     st.session_state.costs = []
 
 
+def select_model():
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-3.5-16k", "GPT-4"))
+    if model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo"
+    elif model == "GPT-3.5-16k":
+        st.session_state.model_name = "gpt-3.5-turbo-16k"
+    else:
+        st.session_state.model_name = "gpt-4"
+    
+    # 300: 本文以外の指示のトークン数 (以下同じ)
+    st.session_state.max_token = OpenAI.modelname_to_contextsize(st.session_state.model_name) - 300
+    return ChatOpenAI(temperature=0, model_name=st.session_state.model_name)
+
+
 def get_pdf_text():
     uploaded_file = st.file_uploader(
         label='Upload your PDF here',
-        type='pdf',
-        #label="読み取ったPDFファイルからあなたの質問に答えます。",
-        #accept_multiple_files='False'
+        type='pdf'
     )
     if uploaded_file:
         pdf_reader = PdfReader(uploaded_file)
         text = '\n\n'.join([page.extract_text() for page in pdf_reader.pages])
         text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-            model_name=st.session_state.emb_model_name,
+            model_name="text-embedding-ada-002",
             # 適切な chunk size は質問対象のPDFによって変わるため調整が必要
             # 大きくしすぎると質問回答時に色々な箇所の情報を参照することができない
             # 逆に小さすぎると一つのchunkに十分なサイズの文脈が入らない
-            chunk_size=250,
+            chunk_size=500,
             chunk_overlap=0,
         )
         return text_splitter.split_text(text)
@@ -78,41 +95,71 @@ def build_vector_store(pdf_text):
     #     pdf_text,
     #     OpenAIEmbeddings(),
     #     path="./local_qdrant",
-    #     collection_name=COLLECTION_NAME,
+    #     collection_name="my_documents",
     # )
 
 
-def page_pdf_upload_and_build_vector_db():
-    st.title("PDF Upload")
-    container = st.container()
-    with container:
-        pdf_text = get_pdf_text()
-        if pdf_text:
-            with st.spinner("Loading PDF ..."):
-                build_vector_store(pdf_text)
+def build_qa_model(llm):
+    qdrant = load_qdrant()
+    retriever = qdrant.as_retriever(
+        # "mmr",  "similarity_score_threshold" などもある
+        search_type="similarity",
+        # 文書を何個取得するか (default: 4)
+        search_kwargs={"k":10}
+    )
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff", 
+        retriever=retriever,
+        return_source_documents=True,
+        verbose=True
+    )
 
 
-def page_ask_my_pdf():
-    st.title("Ask My PDF(s)")
-    st.write('Under Construction')
+def ask(qa, query):
+    with get_openai_callback() as cb:
+        # query / result / source_documents
+        answer = qa(query)
 
-    # 後で実装する
+    return answer, cb.total_cost
 
 
 def main():
     init_page()
 
-    selection = st.sidebar.radio("Go to", ["PDF Upload", "Ask My PDF(s)"])
-    if selection == "PDF Upload":
-        page_pdf_upload_and_build_vector_db()
-    elif selection == "Ask My PDF(s)":
-        page_ask_my_pdf()
+    st.title("Ask My PDF(s)")
+
+    llm = select_model()
+    container = st.container()
+    response_container = st.container()
+
+    with container:
+        pdf_text = get_pdf_text()
+        if pdf_text:
+            with st.spinner("Loading PDF ..."):
+                build_vector_store(pdf_text)
+            
+            query = st.text_input("Query: ", key="input")
+            if query:
+                qa = build_qa_model(llm)
+                if qa:
+                    with st.spinner("ChatGPT is typing ..."):
+                        answer, cost = ask(qa, query)
+                    st.session_state.costs.append(cost)
+                else:
+                    answer = None
+
+                if answer:
+                    with response_container:
+                        st.markdown("## Answer")
+                        st.write(answer)
 
     costs = st.session_state.get('costs', [])
     st.sidebar.markdown("## Costs")
     st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
     for cost in costs:
         st.sidebar.markdown(f"- ${cost:.5f}")
+
 
 if __name__ == '__main__':
     main()
